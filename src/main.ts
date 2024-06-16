09dff9cd82374687c8bcf2bd2c76f969a3c5c9c0
jsm2.pe@gmail.com <jsm2.pe@gmail.com>
2024-06-15 21:30:55 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ENVIRONMENT } from './utils/globals';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
+  app.enableCors({
+    origin: [process.env.APP_URL],
+    credentials: true,
+  });
   await app.listen(port);
-  console.log(`[${ENVIRONMENT}] RUNNING AT PORT: ${port}`)
+  console.log(`[${ENVIRONMENT}] RUNNING AT PORT: ${port}`);
 }
 bootstrap();
